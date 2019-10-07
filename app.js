8c31c3c720e5b0c6113f7dcf1a6af051a810ea02
Duy Nguyen <nguyenngocduy.84@live.com>
2019-10-07 13:02:02 +0700

    stopwatch version 2

--- app.js
+++ app.js
@@ -1,62 +1,48 @@
 var h1 = document.getElementsByTagName('h1')[0],
-  start = document.getElementById('start'),
-  stop = document.getElementById('stop'),
-  resume = document.getElementById('resume'),
-  clear = document.getElementById('clear'),
-  seconds = 0, minutes = 0, hours = 0,
-  t,
-  running;
+  miliseconds = 0, seconds = 0, minutes = 0, hours = 0;
 
 function add() {
-  seconds++;
-  if (seconds >= 60) {
-    seconds = 0;
-    minutes++;
-    if (minutes >= 60) {
+  miliseconds+=11;
+  if (miliseconds >= 999) {
+    miliseconds = 0;
+    seconds++;
+    if (seconds >= 60) {
+      seconds = 0;
+      minutes++;
+      if (minutes >= 60) {
         minutes = 0;
         hours++;
+      }
     }
   }
 
-  h1.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+  h1.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds ? (seconds > 9 ? seconds : "0" + seconds) : "00") + ":" + (miliseconds > 99 ? miliseconds : miliseconds > 9 ? "0" + miliseconds : "00" + miliseconds);
   timer();
 }
 function timer() {
-  t = setTimeout(add, 1000);
-}
-
-var hideButton = function() {
-  if (running === true) {
-    start.style.display = "none";
-    resume.style.display = "inline";
-  } else {
-    start.style.display = "inline";
-    resume.style.display = "none";
-  }
+  window.myTimeout = setTimeout(add, 11);
 }
 
 var startTime = function() {
-  running = false;
   timer();
+  start.style.display = "none";
+}
+
+var resumeTime = function() {
+  timer();
+  resume.style.display = "none";
 }
 
 var stopTime = function() {
-  running = true;
-  clearTimeout(t);
-  hideButton();
+  clearTimeout(window.myTimeout);
+  start.style.display = "none";
+  resume.style.display = "inline";
 }
 
 var clearTime = function() {
-  h1.textContent = "00:00:00";
-  seconds = 0; minutes = 0; hours = 0;
-  running = false;
-  hideButton();
+  h1.textContent = "00:00:00:000";
+  miliseconds = 0; seconds = 0; minutes = 0; hours = 0;
+  clearTimeout(window.myTimeout);
+  start.style.display = "inline";
+  resume.style.display = "none";
 }
-
-start.onclick = startTime;
-
-resume.onclick = startTime;
-
-stop.onclick = stopTime;
-
-clear.onclick = clearTime;
